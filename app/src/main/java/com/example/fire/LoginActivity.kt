314f78c042fe88223d614d42e00c55bc87c16e47
Davinchi-x7 <dufgvjdu@gmail.com>
2023-04-26 06:53:09 -0400

    firebase authentication

---
+++ app/src/main/java/com/example/fire/LoginActivity.kt
@@ -0,0 +1,54 @@
+package com.example.fire
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var Log_email: EditText
+    lateinit var Log_password: EditText
+    lateinit var Log_btn: Button
+    lateinit var Reg_btn: Button
+    lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        Log_email = findViewById(R.id.Mail_field)
+        Log_password = findViewById(R.id.Pazz_field)
+        Log_btn = findViewById(R.id.btn_login)
+        Reg_btn = findViewById(R.id.btn_rg)
+        auth = FirebaseAuth.getInstance()
+
+        Log_btn.setOnClickListener {
+            var email = Log_email.text.toString().trim()
+            var password = Log_password.text.toString().trim()
+
+//validate input
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "cant submit empty field", Toast.LENGTH_SHORT).show()
+            } else {
+
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) {
+                    if (it.isSuccessful) {
+                        Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show()
+
+                        var gotomain = Intent(this, MainActivity::class.java)
+                        startActivity(gotomain)
+                        finish()
+                    } else {
+
+                        Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+
+            }
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/fire/Register.kt
@@ -0,0 +1,55 @@
+package com.example.fire
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class Register : AppCompatActivity() {
+    lateinit var Name: EditText
+    lateinit var Email: EditText
+    lateinit var Password: EditText
+    lateinit var Create: Button
+
+    //initialize firebase
+    lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        Name = findViewById(R.id.full_name)
+        Email = findViewById(R.id.email_add)
+        Password = findViewById(R.id.password)
+        Create = findViewById(R.id.create_btn)
+
+        auth = FirebaseAuth.getInstance()
+
+        //initialize firebase again
+
+
+        Create.setOnClickListener {
+            var name = Name.text.toString().trim()
+            var email = Email.text.toString().trim()
+            var password = Password.text.toString().trim()
+
+            if (name.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Cannot submit an empty field", Toast.LENGTH_SHORT).show()
+            } else {
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) {
+                    if (it.isSuccessful) {
+                        Toast.makeText(this, "User Created Successfully", Toast.LENGTH_SHORT).show()
+                        var gotologin = Intent(this, LoginActivity::class.java)
+                        startActivity(gotologin)
+                        finish()
+                    } else {
+                        Toast.makeText(this, "Failed to create user", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+            }
+        }
+    }
+}
